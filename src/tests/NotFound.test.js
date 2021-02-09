91b1e7cb87fe039e25b3a1d77c3ebfe224eb1958
Leticia <leticia.bora@gmail.com>
2021-02-09 14:08:37 -0300

    teste4lint

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,12 +4,13 @@ import { NotFound } from '../components';
 import renderWithRouter from '../renderWithRouter';
 
 describe('NotFound.js', () => {
-  test('Verifica se a página contém um heading com o texto Page requested not found', () => {
-    const { getByText, history } = renderWithRouter(<App />);
+  test('Verifica se a página contém um heading com o texto Page requested not found',
+    () => {
+      const { getByText, history } = renderWithRouter(<App />);
 
-    history.push('/not-found');
-    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
-  });
+      history.push('/not-found');
+      expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+    });
 
   test('verifica se a página contém o gif', () => {
     const { getByRole, history } = renderWithRouter(<NotFound />);
