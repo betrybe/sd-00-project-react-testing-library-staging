401a9a7eac8f7b4ba6297087e0e071d7b1e11da7
Leticia <leticia.bora@gmail.com>
2021-02-09 11:55:34 -0300

    teste2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,56 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render, screen, waitForElement } from '@testing-library/react';
+import { About } from '../components';
+import App from '../App';
+
+describe('About.js', () => {
+  // test('Verifica se a página contém informações da Pokédex', () => {
+  //   const { getByText, getByRole } = render(
+  //     <MemoryRouter initialEntries={ ['/about'] }>
+  //       <About />
+  //     </MemoryRouter>,
+  //   );
+
+  //   // const title = getByText(/About Pokédex/i);
+  //   const heading = getByRole('heading', { level: 2 });
+  //   // expect(title).toBeInTheDocument();
+  //   expect(heading).toHaveTextContent(/About Pokédex/i);
+// })
+
+  test('Verifica se a página contém informações da Pokédex', () => {
+    const { getByText, container, getByRole } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const aboutPage = getByText(/About/i);
+    fireEvent.click(aboutPage);
+
+    // const header = container.querySelector('h2');
+    // expect(header).toHaveTextContent('About Pokédex');
+    const header = getByRole('heading', { level: 2 });
+    expect(header).toHaveTextContent('About Pokédex');
+  });
+
+  test('Verifica se a página contém dois parágrafos sobre a pokedéx', () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <About />
+      </MemoryRouter>,
+    );
+    const paragraph = container.querySelectorAll('p');
+    expect(paragraph).toHaveLength(2);
+  });
+
+  test('Verifica se possui a imagem de uma pokedéx', () => {
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={ ['/about'] }>
+        <About />
+      </MemoryRouter>,
+    );
+    const image = getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,88 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { fireEvent, render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import App from '../App';
+import pokemons from '../data';
+
+describe('FavoritePokemons.js', () => {
+  test('verifica se não tem nenhum pokémon favoritado', () => {
+    const { getByText, container } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    fireEvent.click(favorite);
+
+    const title = container.querySelector('h2');
+    expect(title).toHaveTextContent(/Favorite Pokémons/i);
+
+    const noFavorite = getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+
+  test('verifica se o pokémon foi favoritado', () => {
+    // const { getByText, getByLabelText } = render(
+    //   <MemoryRouter initialEntries={ ['/'] }>
+    //     <App />
+    //   </MemoryRouter>,
+    // );
+
+    // const moreDetails = getByText(/More details/i);
+    // fireEvent.click(moreDetails);
+
+    // const getLabel = getByLabelText(/Pokémon favoritado?/i);
+    // expect(getLabel).toBeInTheDocument();
+
+    // fireEvent.click(getLabel);
+    // fireEvent.click(getByText(/Favorite Pokémons/i));
+
+    // const pokemon = getByText(/Pikachu/i);
+    // expect(pokemon).toBeInTheDocument();
+    const { getByText, getByLabelText } = render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ pokemons } />
+      </MemoryRouter>,
+    );
+
+    const favorite = getByText(/Pikachu/i);
+    expect(favorite).toBeInTheDocument();
+  });
+
+  test('verifica se não exibe o pokémon não favoritado', () => {
+    const pokemon = [{
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+
+    }];
+    const { queryByText, getByLabelText } = render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ pokemon } />
+      </MemoryRouter>,
+    );
+
+    //queryBy evita que cuspa o erro, diferente de getBy
+
+    const noFavorite = queryByText(/Charmander/i);
+    expect(noFavorite).not.toBeInTheDocument();
+  });
+});
