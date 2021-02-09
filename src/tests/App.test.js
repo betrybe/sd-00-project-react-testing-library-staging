87d86fbdc0b1d396b1a12906112b91904c54d4c2
Leticia <leticia.bora@gmail.com>
2021-02-09 11:45:14 -0300

    teste

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,12 +3,31 @@ import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders a reading with the text `Pokédex`', () => {
-  const { getByText } = render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const heading = getByText(/Pokédex/i);
-  expect(heading).toBeInTheDocument();
+describe('App.js', () => {
+  test('shows the Pokédex when the route is `/`', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+    const heading = getByText(/Pokédex/i);
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('verifica se a rota da navbar é "/"', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <App />
+      </MemoryRouter>,
+    );
+
+    const home = getByText(/Home/i);
+    expect(home).toHaveAttribute('href', '/');
+
+    const about = getByText(/About/i);
+    expect(about).toHaveAttribute('href', '/about');
+
+    const favorite = getByText(/Favorite Pokémons/i);
+    expect(favorite).toHaveAttribute('href', '/favorites');
+  });
 });
