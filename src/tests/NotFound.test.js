40b552fe3cd85133564d4c81ed929ad07fd01851
Leticia <leticia.bora@gmail.com>
2021-02-09 12:59:23 -0300

    teste4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('NotFound.js', () => {
+  test('Verifica se a página contém um heading com o texto Page requested not found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+
+    history.push('/not-found');
+    expect(getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+
+  test('verifica se a página contém o gif', () => {
+    const { getByRole, history } = renderWithRouter(<NotFound />);
+
+    history.push('/not-found');
+    const path = 'Pikachu crying because the page requested was not found';
+    const image = getByRole('img', { name: path });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
